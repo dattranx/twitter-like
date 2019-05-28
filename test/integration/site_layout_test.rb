34ff7f8ae767b9c7f10f0bb8c60a6c597f10b247
dattranx <45516672+dattranx@users.noreply.github.com>
2019-05-28 21:48:04 +0700

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,11 +2,15 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+  test "layout links for non-logged-in users" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
@@ -15,5 +19,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
-  
+
+  test "layout links for logged-in users" do
+    log_in_as(@user, remember_me: '0')
+    get root_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    get users_path
+    assert_select "title", full_title("All users")
+    get user_path(@user)
+    assert_select "title", full_title(@user.name)
+  end
 end
